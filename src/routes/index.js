0728692529c386cd18d82fcd5cfa981629d19f2b
Mackinder, Patrick <paddymackinder@gmail.com>
2016-09-20 22:03:20 +0100

    feat(): Add react app seed created by create-react-app. Remove jade as configured view engine. Now serves the index page of create-react-app build. PM

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  res.sendFile(path.resolve(__dirname + '/../public/build/index.html'));
 });
 
 module.exports = router;
