e78f697f97634559f5ee24725fff6e8b01703636
video <video>
2017-01-19 16:17:16 +0000

    don't capitalize first letter and replace underscores with spaces

--- src/public/src/util/NameFormatting.js
+++ src/public/src/util/NameFormatting.js
@@ -1,4 +1,5 @@
 export const splitOnDashes = (name) =>
-    name.split('-')
-    .map(word => word.charAt(0).toUpperCase() + word.substring(1))
-    .join(' ');
+    name
+    .split('-')
+    .join(' ')
+    .replace('_', ' ');
