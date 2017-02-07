2b9ce89aeef969321d61324c3ffa5904e502ec25
Mackinder, Patrick <paddymackinder@gmail.com>
2017-02-07 11:48:01 +0000

    refactor(): Change from function prototypes to classes. PM

--- src/data/Graph.js
+++ src/data/Graph.js
@@ -1,117 +1,121 @@
-var Graph = function() {
-    this.nodes = {};
-    this.source = undefined;
-};
-
-Graph.prototype.getNodes = function() {
-    return Object.keys(this.nodes);
-};
-
-Graph.prototype.getNode = function(id) {
-    return this.nodes[id];
-};
-
-Graph.prototype.getLinks = function(id) {
-    if(this.nodes.hasOwnProperty(id)) {
-        return this.nodes[id].links;
-    }
-};
-
-function createNodesThatDontExist(graph, links) {
-    links.forEach(function(link) {
-        if(!graph.nodes.hasOwnProperty(link)) {
-            graph.addNode(link, {});
-        }
-    });
-}
-
-Graph.prototype.addLinks = function(id, links) {
-    if(this.nodes.hasOwnProperty(id)) {
-        createNodesThatDontExist(this, links);
-        var node = this.nodes[id];
-        links.forEach(function(link) {
-            if(node.links.indexOf(link) < 0) {
-               node.links.push(link);
-            }
-        })
-    }
-};
-
-Graph.prototype.addData = function(id, newData) {
-    if(this.nodes.hasOwnProperty(id)) {
-        let currentData = this.nodes[id].data;
-        Object.assign(currentData, newData);
-    }
-};
-
-Graph.prototype.addNode = function(id, data, links) {
-    if(links) {
-        createNodesThatDontExist(this, links);
+class Graph {
+    constructor() {
+        this.nodes = {};
+        this.source = undefined;
     }
 
-    if(this.nodes[id]) {
-        this.nodes[id] = {
-            data: data,
-            links: links || this.nodes[id].links
-        }
-    } else {
-        this.nodes[id] = {
-            data: data,
-            links: links || []
-        }
+    getNodes() {
+        return Object.keys(this.nodes);
     }
-};
 
-Graph.prototype.removeNode = function(id) {
-    for(var nodeId in this.nodes) {
-        var node = this.nodes[nodeId];
-        var idx = node.links.indexOf(id);
-        if(idx > -1) {
-            node.links.splice(idx, 1);
+    getNode(id) {
+        return this.nodes[id];
+    }
+
+    getLinks(id) {
+        if(this.nodes.hasOwnProperty(id)) {
+            return this.nodes[id].links;
         }
     }
 
-    delete this.nodes[id];
-};
-
-Graph.prototype.breadthFirstSearch = function(origin) {
-    var result = [origin];
-    var queue = [origin];
-    while(queue.length > 0) {
-        this.nodes[queue.shift()].links.sort().forEach(function(link) {
-            if(result.indexOf(link) < 0) {
-                result.push(link);
-                queue.push(link);
+    _createNodesThatDontExist(graph, links) {
+        links.forEach(link => {
+            if(!graph.nodes.hasOwnProperty(link)) {
+                graph.addNode(link, {});
             }
         });
     }
 
-    return result;
-};
-
-Graph.prototype.size = function() {
-    return Object.keys(this.nodes).length;
-};
-
-Graph.prototype.addSourceNode = function(id, data, links) {
-    this.addNode(id, data, links);
-    this.source = id;
-};
-
-Graph.prototype.getSource = function() {
-    return this.source;
-};
-
-Graph.prototype.toJson = function() {
-    return {
-        nodes: JSON.parse(JSON.stringify(this.nodes)),
-        source: this.source
+    addLinks(id, links) {
+        if(this.nodes.hasOwnProperty(id)) {
+            this._createNodesThatDontExist(this, links);
+            let node = this.nodes[id];
+            links.forEach(link => {
+                if(node.links.indexOf(link) < 0) {
+                    node.links.push(link);
+                }
+            });
+        }
     }
-};
 
-Graph.prototype.fromJson = function(graph) {
-    this.nodes = JSON.parse(JSON.stringify(graph.nodes));
-    this.source = graph.source;
-};
+    addData(id, newData) {
+        if(this.nodes.hasOwnProperty(id)) {
+            let currentData = this.nodes[id].data;
+            Object.assign(currentData, newData);
+        }
+    }
+
+    addNode(id, data, links) {
+        if(links) {
+            this._createNodesThatDontExist(this, links);
+        }
+
+        if(this.nodes[id]) {
+            this.nodes[id] = {
+                data: data,
+                links: links || this.nodes[id].links
+            }
+        } else {
+            this.nodes[id] = {
+                data: data,
+                links: links || []
+            }
+        }
+    }
+
+    removeNode(id) {
+        for (let nodeId in this.nodes) {
+            let node = this.nodes[nodeId],
+                idx = node.links.indexOf(id);
+
+            if(idx > -1) {
+                node.links.splice(idx, 1);
+            }
+        }
+
+        delete this.nodes[id];
+    }
+
+    breadthFirstSearch(origin) {
+        let result = [origin],
+            queue = [origin];
+
+        while (queue.length > 0) {
+            this.nodes[queue.shift()].links.sort().forEach(link => {
+                if(result.indexOf(link) < 0) {
+                    result.push(link);
+                    queue.push(link);
+                }
+            });
+        }
+
+        return result;
+    }
+
+    size() {
+        return Object.keys(this.nodes).length;
+    }
+
+    addSourceNode(id, data, links) {
+        this.addNode(id, data, links);
+        this.source = id;
+    }
+
+    getSource() {
+        return this.source;
+    }
+
+    toJson() {
+        return {
+            nodes: JSON.parse(JSON.stringify(this.nodes)),
+            source: this.source
+        }
+    }
+
+    fromJson(graph) {
+        this.nodes = JSON.parse(JSON.stringify(graph.nodes));
+        this.source = graph.source;
+    }
+}
 
 module.exports = Graph;
--- test/utils/GocdResponseBuilder.js
+++ test/utils/GocdResponseBuilder.js
@@ -1,219 +1,198 @@
-goCdResponseBuilder = {};
-
-var StageBuilder = function() {
-    this.name = "Build";
-    this.result = "Passed";
-};
-
-StageBuilder.prototype.withName = function(name) {
-    this.name = name;
-    return this;
-};
-
-StageBuilder.prototype.withResult = function(result) {
-    this.result = result;
-    return this;
-};
-
-StageBuilder.prototype.withLabel = function(counter) {
-    this.counter = counter;
-    return this;
-};
-
-StageBuilder.prototype.build = function() {
-    var stage = this;
-    return {
-        name: stage.name,
-        result: stage.result
+class StageBuilder {
+    constructor() {
+        this.name = "Build";
+        this.result = "Passed";
     }
-};
 
-var PipelineHistoryBuilder = function() {
-    this.pipelines = [new PipelineBuilder()]
-};
-
-PipelineHistoryBuilder.prototype.withPipeline = function(pipeline) {
-    this.pipelines = [pipeline];
-    return this;
-};
-
-PipelineHistoryBuilder.prototype.withNoPipelines = function() {
-    this.pipelines = [];
-    return this;
-};
-
-PipelineHistoryBuilder.prototype.build = function() {
-    var pipelines = [];
-    this.pipelines.forEach(function(pipeline) {
-        pipelines.push(pipeline.build())
-    });
-    return {
-        pipelines: pipelines
+    withName(name) {
+        this.name = name;
+        return this;
     }
-};
 
-var PipelineBuilder = function() {
-    this.name = "Dev";
-    this.stages = [new StageBuilder()];
-    this.label = 1;
-    this.materials = [];
-};
-
-PipelineBuilder.prototype.withName = function(name) {
-    this.name = name;
-    return this;
-};
-
-PipelineBuilder.prototype.withLabel = function(label) {
-    this.label = label;
-    return this;
-};
-
-PipelineBuilder.prototype.withStage = function(stage) {
-    this.stages = [stage];
-    return this;
-};
-
-PipelineBuilder.prototype.addStage = function(stage) {
-    this.stages.push(stage);
-    return this;
-};
-
-PipelineBuilder.prototype.withMaterial = function(material) {
-    this.materials = [material];
-    return this;
-};
-
-PipelineBuilder.prototype.addMaterial = function(material) {
-    this.materials.push(material);
-    return this;
-};
-
-PipelineBuilder.prototype.build = function() {
-    const pipeline = this;
-    let stages = [],
-        materials = [];
-
-    this.stages.forEach(function(stage) {
-        stages.push(stage.build())
-    });
-
-    this.materials.forEach(function(material) {
-        materials.push(material.build())
-    });
-
-    return {
-        name: pipeline.name,
-        stages,
-        label: pipeline.label,
-        materials
+    withResult(result) {
+        this.result = result;
+        return this;
     }
-};
 
-var PipelineSummaryBuilder = function() {
-    this.name = "Dev";
-    this.stages = [{name: "Build"}];
-};
-
-PipelineSummaryBuilder.prototype.withName = function(name) {
-    this.name = name;
-};
-
-PipelineSummaryBuilder.prototype.withStage = function(stage) {
-    this.stages = [{name: stage}];
-    return this;
-};
-
-PipelineSummaryBuilder.prototype.addStage = function(stage) {
-    this.stages.push({name: stage});
-    return this;
-};
-
-PipelineSummaryBuilder.prototype.build = function() {
-    var pipelineSummary = this;
-    return {
-        name: pipelineSummary.name,
-        stages: pipelineSummary.stages
+    build() {
+        return {
+            name: this.name,
+            result: this.result
+        }
     }
-};
+}
 
-var PipelineGroupBuilder = function() {
-    this.name = "Application";
-    this.pipelines = [new PipelineSummaryBuilder()]
-};
-
-PipelineGroupBuilder.prototype.withName = function(name) {
-    this.name = name;
-    return this;
-};
-
-PipelineGroupBuilder.prototype.addPipeline = function(pipeline) {
-    this.pipelines.push(pipeline);
-    return this;
-};
-
-PipelineGroupBuilder.prototype.withPipeline = function(pipeline) {
-    this.pipelines = [pipeline];
-    return this;
-};
-
-PipelineGroupBuilder.prototype.build = function() {
-    var group = this;
-    var pipelines = [];
-    this.pipelines.forEach(function(pipeline) {
-        pipelines.push(pipeline.build())
-    });
-    return {
-        name: group.name,
-        pipelines: pipelines
+class PipelineHistoryBuilder {
+    constructor() {
+        this.pipelines = [new PipelineBuilder()]
     }
-};
 
-var PipelineGroupsBuilder = function() {
-    this.groups = [new PipelineGroupBuilder()]
-};
-
-PipelineGroupsBuilder.prototype.addGroup = function(group) {
-    this.groups.push(group);
-    return this;
-};
-
-PipelineGroupsBuilder.prototype.withGroup = function(group) {
-    this.groups = [group];
-    return this;
-};
-
-
-PipelineGroupsBuilder.prototype.build = function() {
-    var groups = [];
-    this.groups.forEach(function(group) {
-        groups.push(group.build())
-    });
-    return groups
-};
-
-var MaterialBuilder = function() {
-    this.description = "upstream-pipeline";
-    this.type = "Pipeline";
-};
-
-MaterialBuilder.prototype.withDescription = function(description) {
-    this.description = description;
-    return this;
-};
-
-MaterialBuilder.prototype.withType = function(type) {
-    this.type = type;
-    return this;
-};
-
-MaterialBuilder.prototype.build = function() {
-    var material = this;
-    return {
-        "description": material.description,
-        "type": material.type
+    withPipeline(pipeline) {
+        this.pipelines = [pipeline];
+        return this;
     }
-};
+
+    withNoPipelines() {
+        this.pipelines = [];
+        return this;
+    }
+
+    build() {
+        return {
+            pipelines: this.pipelines.map(pipeline => pipeline.build())
+        }
+    }
+}
+
+class PipelineBuilder {
+    constructor() {
+        this.name = "Dev";
+        this.stages = [new StageBuilder()];
+        this.label = 1;
+        this.materials = [];
+    }
+
+    withName(name) {
+        this.name = name;
+        return this;
+    }
+
+    withLabel(label) {
+        this.label = label;
+        return this;
+    }
+
+    withStage(stage) {
+        this.stages = [stage];
+        return this;
+    }
+
+    addStage(stage) {
+        this.stages.push(stage);
+        return this;
+    }
+
+    withMaterial(material) {
+        this.materials = [material];
+        return this;
+    }
+
+    addMaterial(material) {
+        this.materials.push(material);
+        return this;
+    }
+
+    build() {
+        return {
+            name: this.name,
+            stages: this.stages.map(stage => stage.build()),
+            label: this.label,
+            materials: this.materials.map(material => material.build())
+        }
+    }
+}
+
+class PipelineSummaryBuilder {
+    constructor() {
+        this.name = "Dev";
+        this.stages = [{name: "Build"}];
+    }
+
+    withName(name) {
+        this.name = name;
+    }
+
+    withStage(stage) {
+        this.stages = [{name: stage}];
+        return this;
+    }
+
+    addStage(stage) {
+        this.stages.push({name: stage});
+        return this;
+    }
+
+    build() {
+        return {
+            name: this.name,
+            stages: this.stages
+        }
+    }
+}
+
+class PipelineGroupBuilder {
+    constructor() {
+        this.name = "Application";
+        this.pipelines = [new PipelineSummaryBuilder()]
+    }
+
+    withName(name) {
+        this.name = name;
+        return this;
+    }
+
+    addPipeline(pipeline) {
+        this.pipelines.push(pipeline);
+        return this;
+    }
+
+    withPipeline(pipeline) {
+        this.pipelines = [pipeline];
+        return this;
+    }
+
+    build() {
+        return {
+            name: this.name,
+            pipelines: this.pipelines.map(pipeline => pipeline.build())
+        }
+    }
+}
+
+class PipelineGroupsBuilder {
+    constructor() {
+        this.groups = [new PipelineGroupBuilder()]
+    }
+
+    addGroup(group) {
+        this.groups.push(group);
+        return this;
+    }
+
+    withGroup(group) {
+        this.groups = [group];
+        return this;
+    }
+
+
+    build() {
+        return this.groups.map(group => group.build())
+    }
+}
+
+class MaterialBuilder {
+    constructor() {
+        this.description = "upstream-pipeline";
+        this.type = "Pipeline";
+    }
+
+    withDescription(description) {
+        this.description = description;
+        return this;
+    }
+
+    withType(type) {
+        this.type = type;
+        return this;
+    }
+
+    build() {
+        return {
+            description: this.description,
+            type: this.type
+        }
+    }
+}
 
 module.exports = {
     StageBuilder,
